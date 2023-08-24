e388a9c0e211a4f9e8b18dccb1c78ff50e62fe8e
FabienDumont <fabdum19@live.fr>
2023-08-24 16:33:57 +0200

    First Commit

---
+++ FirebaseAdminAuthentification.DependencyInjection/Extensions/AddFirebaseAuthentificationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FirebaseAdminAuthentification.DependencyInjection.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FirebaseAdminAuthentification.DependencyInjection.Extensions;
+
+public static class AddFirebaseAuthentificationExtensions {
+    public static IServiceCollection AddFirebaseAuthentication(this IServiceCollection services) {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddScheme<AuthenticationSchemeOptions, FirebaseAuthenticationHandler>(JwtBearerDefaults.AuthenticationScheme, (o) => { });
+
+        services.AddScoped<FirebaseAuthenticationFunctionHandler>();
+
+        return services;
+    }
+}
